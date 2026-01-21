64f64e7556adae9a9c5d7557e387e1dcc278ef34
karimlifatimaa <kerimlifatime417@gmail.com>
2026-01-21 15:10:01 +0400

    add webhook

--- src/main/java/com/woofly/companymanagementapp/controller/GitHubWebhookController.java
+++ src/main/java/com/woofly/companymanagementapp/controller/GitHubWebhookController.java
@@ -1,5 +1,7 @@
 package com.woofly.companymanagementapp.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +14,28 @@ public class GitHubWebhookController {
     @PostMapping("/github")
     public ResponseEntity<String> handleGitHubEvent(@RequestBody String payload,
                                                     @RequestHeader("X-GitHub-Event") String eventType) {
-        log.info("------- YENİ BİLDİRİŞ GƏLDİ! -------");
-        log.info("Hadisə növü: {}", eventType);
-        log.debug("Gələn Məlumat (Payload): {}", payload);
-        log.info("------------------------------------");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(payload);
 
-        // GitHub-a hər şeyin qaydasında olduğunu bildirmək üçün 200 OK qaytarırıq
+            String pusherName = root.path("pusher").path("name").asText();
+
+            String branch = root.path("ref").asText();
+
+            String commitMessage = root.path("head_commit").path("message").asText();
+
+            String repoName = root.path("repository").path("name").asText();
+
+            log.info("======= GITHUB MƏLUMATI =======");
+            log.info("Repozitoriya: {}", repoName);
+            log.info("İstifadəçi: {}", pusherName);
+            log.info("Branch: {}", branch);
+            log.info("Mesaj: {}", commitMessage);
+            log.info("===============================");
+
+        } catch (Exception e) {
+            log.error("Parsing xətası: {}", e.getMessage());
+        }
         return ResponseEntity.ok("Webhook uğurla alındı!");
     }
 }
